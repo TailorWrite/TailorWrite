e2b19e5c604a0cd97490b1320d7d8be8ed3d521a
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2024-07-25 13:25:21 +1200

    chore: Update frontend build configuration and set base path for GitBucket pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/info301/TailorWrite/TailorWrite/pages/",
   build: {
     outDir: '../docs',
     emptyOutDir: true, // also necessary
