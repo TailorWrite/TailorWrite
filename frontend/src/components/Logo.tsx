2600d65e1c81751c5c9c5b60c5c96d9815912f27
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2024-07-31 11:58:21 +1200

    chore(ui): Update Logo

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,14 +1,21 @@
+import { BriefcaseIcon, CubeTransparentIcon, FingerPrintIcon } from "@heroicons/react/24/solid";
+
 interface LogoProps {
     className?: string;
 }
 
+
+
 export default function Logo({ className }: LogoProps) {
     return (
-        <img
-            alt="TailorWrite logo"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            // className={`h-8 w-auto ${className}`}
-            className={className}
-        />
+
+        <BriefcaseIcon className={`${className} text-sky-400`} />
+
+        // <img
+        //     alt="TailorWrite logo"
+        //     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+        //     // className={`h-8 w-auto ${className}`}
+        //     className={className}
+        // />
     );
 }
