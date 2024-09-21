86e3e971662dbc357c171827a06bb541279290f0
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2024-09-22 01:12:00 +1200

    docs: Document ApplicationLayout component

--- frontend/src/layouts/ApplicationsLayout.tsx
+++ frontend/src/layouts/ApplicationsLayout.tsx
@@ -2,6 +2,15 @@ import { Outlet } from 'react-router-dom';
 
 import ApplicationTrackerView from '../views/ApplicationTracker';
 
+/**
+ * The `ApplicationsLayout` function renders the ApplicationTrackerView component
+ * and provides an outlet for displaying the Single Application Drawer.
+ * 
+ * @return The `ApplicationsLayout` component is being returned. It includes a
+ * `div` element with the id "applications-layout" and the class "h-full". Inside
+ * the `div`, the `ApplicationTrackerView` component is rendered, and an `Outlet`
+ * component is used for displaying the Single Application Drawer.
+ */
 function ApplicationsLayout() {
     return (
         <div id="applications-layout" className="h-full">
