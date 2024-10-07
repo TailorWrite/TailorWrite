8464e2e2e1fa2d3fbb4a0aa374eb5270fdf932dd
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2024-10-07 21:15:27 +1300

    fix: Replace the environment variable for API_BASE_PATH

--- frontend/src/pathConstants.ts
+++ frontend/src/pathConstants.ts
@@ -17,7 +17,7 @@ const PathConstants = {
     SETTINGS_DATA: '/dashboard/settings/data',
 }
 
-const API_BASE_URL = "http://localhost:5001";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 console.log('API_BASE_URL:', API_BASE_URL);
 const APIConstants = {
     BASE_URL: API_BASE_URL,
