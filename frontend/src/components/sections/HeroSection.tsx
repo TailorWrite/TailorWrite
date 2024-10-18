d8b1f8111f5afa3ce549ff60c496373a53d620e1
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2024-10-18 16:41:41 +1300

    fix: Update LandingPage links

--- frontend/src/components/sections/HeroSection.tsx
+++ frontend/src/components/sections/HeroSection.tsx
@@ -1,4 +1,7 @@
+import { Link } from 'react-router-dom';
+
 import Nav from '../common/Nav'; 
+import PathConstants from '../../pathConstants';
 
 export default function Example() {
     
@@ -33,15 +36,15 @@ export default function Example() {
                             Edit and customize your cover letter to match your preferences and specific job requirements.
                         </p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
-                            <a
-                                href="#"
+                            <Link
+                                to={PathConstants.SIGNUP}
                                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Get started
-                            </a>
-                            <a href="#" className="text-sm font-semibold leading-6 text-gray-900 dark:text-white">
+                            </Link>
+                            {/* <Link to={PathConstants.SIGNUP} className="text-sm font-semibold leading-6 text-gray-900 dark:text-white">
                                 Learn more <span aria-hidden="true">→</span>
-                            </a>
+                            </Link> */}
                         </div>
                     </div>
                 </div>
